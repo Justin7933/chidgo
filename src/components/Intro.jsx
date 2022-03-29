d9b3a75ba6e91c34a3388c1831a69f624d71639f
Justin Damon <justindamon84@gmail.com>
2022-03-29 16:56:11 -0400

    started application, navbar added, intro started

---
+++ src/components/Intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: calc(100vh - 50px);
+    display: flex;
+`;
+
+const Left = styled.div`
+    width: 60%;
+`;
+
+const Right = styled.div`
+    width: 40%;
+`;
+
+const Intro = () => {
+  return ( 
+    <Container></Container>
+  )
+};
+
+export default Intro
